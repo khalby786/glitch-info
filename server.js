dc7495f949d4289eb067252f5ddd074b0208fe88
Glitch (hello-express) <none>
2020-03-28 08:14:28 +0000

    ⚓️🖇 Checkpoint ./server.js:3168504/273 ./README.md:3168504/1654 ./package.json:3168504/12

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const fetch = require('node-fetch');
 module.exports = {
   
   user: function (user) {
+    
+    // check if user parameter is undefined or nothing
+    if (user == undefined || user == "") {
+      // give an error!
+      return console.error("Enter a valid username")
+    }
+    
+    // fetch the glitch api for username
     fetch(`https://api.glitch.com/v1/users/by/login?login=${user}`)
     .then(res => res.json())
     .then(json => console.log(json));
+    
   }
   
 }
